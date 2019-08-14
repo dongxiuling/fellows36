8c8e854794a3cf33a36084c0f6e57ba21a524963
dongxiuling <644961699@qq.com>
2019-08-14 14:12:48 +0800

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');//node 下path
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry:{
         // ./当前路径
@@ -8,6 +9,15 @@ module.exports = {
         // 出口文件路径绝对路径
         path:path.resolve(__dirname,'dist'),
         filename:'[name].js'
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            minify:{
+                removeAttributeQuotes:true
+            },
+            hash:true,
+            template:'./src/index.html'
+        })
+    ]
 }
 
