e066f390dec4d77a5260638b564557a74ea7b5bd
dongxiuling <644961699@qq.com>
2019-08-15 10:19:52 +0800

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,67 +2,73 @@ const path = require('path');//node 下path
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-    mode:"development",//production
-    entry:{
+    mode: "development",//production
+    entry: {
         // ./当前路径
-        index:'./src/index.js'
+        index: './src/index.js'
     },
-    output:{
+    output: {
         // 出口文件路径绝对路径
-        path:path.resolve(__dirname,'dist'),
-        filename:'[name].js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css$/,
-                use:ExtractTextPlugin.extract({
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
-                    use: "css-loader"
+                    use: [
+                        { 
+                            loader: 'css-loader',
+                            options: { importLoaders: 1 } 
+                        },
+                        'postcss-loader'
+                    ]
                 })
-            },{
-                test:/\.(jpg|png|gif)/,
-                use:[
+            }, {
+                test: /\.(jpg|png|gif)/,
+                use: [
                     {
-                        loader:"url-loader",
-                        options:{
-                            limit:500,
-                            outputPath:'images/'
+                        loader: "url-loader",
+                        options: {
+                            limit: 500,
+                            outputPath: 'images/'
                         }
                     }
 
                 ]
             },
             {
-                test:/\.(htm|html)$/i,
-                loader:["html-withimg-loader"]
+                test: /\.(htm|html)$/i,
+                loader: ["html-withimg-loader"]
             },
             // ,{
             //     test:/\.(scss|sass)$/,
             //     use:["style-loader","css-loader","sass-loader"]
             // },
             {
-                test:/\.(scss|sass)$/,
-                use:ExtractTextPlugin.extract({
+                test: /\.(scss|sass)$/,
+                use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
-                    use: ["css-loader","sass-loader"]
+                    use: ["css-loader", "sass-loader"]
                 })
             }
         ]
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            minify:{
-                removeAttributeQuotes:true
+            minify: {
+                removeAttributeQuotes: true
             },
-            hash:true,
-            template:'./src/index.html'
+            hash: true,
+            template: './src/index.html'
         }),
         new ExtractTextPlugin("styles.css"),
     ],
-    devServer:{
-        contentBase:path.resolve(__dirname,'dist'),
-        host:'localhost'
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        host: 'localhost'
     }
 }
 
