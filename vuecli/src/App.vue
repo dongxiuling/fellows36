b12c771c16af205508563ee5a5addfd21d0b5dce
dongxiuling <644961699@qq.com>
2019-09-21 10:23:53 +0800

    router

---
+++ vuecli/src/App.vue
@@ -0,0 +1,29 @@
+<!-- 总组件 -->
+<template>
+  <div id="app">
+    <!-- <common-header></common-header> -->
+    <router-view/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>|
+      <router-link to="/mine">我的 </router-link>|
+      <router-link to="/test">test</router-link>
+    </div>
+  
+    
+  </div>
+</template>
+
+<script>
+    // 1 引入  2components下声明
+    import CommonHeader from './components/CommonHeader';
+    export default {
+        components:{
+            CommonHeader
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
--- vuecli/src/views/Mine.vue
+++ vuecli/src/views/Mine.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-
-  </div>
+    <div>
+        我的页面
+    </div>
 </template>
 
 <script>
-  export default {
-      
-  }
+    export default {
+        
+    }
 </script>
 
 <style lang="scss" scoped>
