b5f18c7594c7cc6d09f1ecd8a6fcdcfd3a45415b
dongxiuling <644961699@qq.com>
2019-09-21 11:23:50 +0800

    路由传参

--- vuecli/src/App.vue
+++ vuecli/src/App.vue
@@ -6,13 +6,15 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>|
       <!-- <router-link to="/mine">我的 </router-link>| -->
+      <!-- params传参 不能使用path直接传 需要使用name -->
       <router-link :to="{name:'mine',params:{id:666,name:'小王'}}">我的params </router-link>|
+      <!-- query传参 -->
       <router-link :to="{path:'/mine',query:{id:666,name:'小王'}}">我的query </router-link>|
+      <!-- 通过路径传参 -->
       <router-link to="/test/5/lisi">test</router-link>
     </div>
     <router-view/>
 
-  
   </div>
 </template>
 
