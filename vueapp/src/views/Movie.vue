abc3d3363ad284ffb6dd0cc3145a5ea4f0a6b5f8
dongxiuling <644961699@qq.com>
2019-09-28 14:27:59 +0800

    电影列表 loading

--- vueapp/src/views/Movie.vue
+++ vueapp/src/views/Movie.vue
@@ -12,6 +12,7 @@
                 </div>
             </li>
         </ul>
+        <img class="loading" v-show="isShow" src="@/assets/images/loading.gif" alt="">
     </div>
 </template>
 
@@ -20,29 +21,43 @@
     export default {
         data(){
             return {
-                movieList:[]
+                movieList:[],
+                isShow:false, //true 显示loading   false不显示loading  
+                flag:true//是否可以再请求数据  true可以请求  false 不可以请求
             }
         },
         created(){
             // 发送请求 
-            // axios.get('https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=0&count=10')
-            axios.get('./data/movie0.json')
-            .then((res)=>{
-                this.movieList= res.data.subjects;
-            }).catch((err)=>{
-                console.log(err);
-            })
-           
-            // window.onscroll = function(){
-            //      // 判断滚动条是否滑动到底部  
-            //       axios.get('https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=0&count=10')
-            //     .then((res)=>{
-            //         this.movieList= res.data.subjects;
-            //     }).catch((err)=>{
-            //         console.log(err);
-            //     })
-            // }
-
+            this.getMovieList(this.movieList.length);
+            window.onscroll = () => {
+                let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+                let clientHeight = document.documentElement.clientHeight;
+                let scrollHeight = document.documentElement.scrollHeight;
+                console.log(scrollHeight,scrollTop ,clientHeight);
+                if( Math.abs(scrollHeight - scrollTop - clientHeight) <1 ){
+                    // 到底
+                    this.getMovieList(this.movieList.length);
+                }
+            }
+        },
+        methods:{
+            getMovieList(num){
+                this.isShow = true;//loading  正在请求 
+                if(this.flag){
+                    this.flag = false;//不可以请求
+                     axios.get('https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start='+num+'&count=10')
+                    // axios.get('https://bird.ioliu.cn/v1?url=http://douban.uieee.com/v2/movie/in_theaters?&start=0&count=10')
+                    // axios.get('./data/movie0.json')
+                    .then((res)=>{
+                        this.movieList = [...this.movieList,...res.data.subjects];
+                        this.isShow = false;
+                        this.flag = true; //可请求  
+                    }).catch((err)=>{
+                        console.log(err);
+                    })
+                }
+               
+            }
         }
     }
 </script>
@@ -60,6 +75,12 @@
         border-bottom: 1px solid #000;
     }
 }
- 
-
+.loading{
+    width:1.5rem;
+    // height:3rem;
+    position: fixed;
+    left:50%;
+    top:50%;
+    transform: translate(-50%,-50%);
+}
 </style>
