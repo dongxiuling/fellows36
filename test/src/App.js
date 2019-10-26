a127d578904e4893f4f88b785abf51d0a3ee6720
dongxiuling <644961699@qq.com>
2019-10-26 16:48:29 +0800

    react

---
+++ test/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+        <Header></Header>
+        hahahahahhad
+        fjdgsfhp  
+        <p>dytfugihojkl'</p>
+        <h3>hahahah</h3>
+        <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
