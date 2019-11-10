b23a87c140d18fca85835b74a844ef08e3bb5b66
dongxiuling <644961699@qq.com>
2019-11-10 13:16:59 +0800

    mock

--- antd-demo/src/Mock/mock.js
+++ antd-demo/src/Mock/mock.js
@@ -1,14 +1,14 @@
 import Mock from 'mockjs';
 var Random = Mock.Random;
 
-// function test(){
-//     for()
-// }
-
 Mock.mock('http://www.weichuang.com/student',{
     'list|1-10': [{
         // 属性 id 是一个自增数，起始值为 1，每次增 1
-        'id|+1': 1,
-        'name':Random.name()
+        'key|+1': 1,
+        'name':'@cname',
+        'address':'@province',
+        'age':'@integer(18,28)',
+        'birthday':'@date'
     }]
 });
+
--- antd-demo/src/page/Student.js
+++ antd-demo/src/page/Student.js
@@ -3,27 +3,26 @@ import {Table} from 'antd';
 import './Student.css';
 import Mock from './../Mock/mock'
 import axios from 'axios'
-const dataSource = [
-    {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号',
-    },
-    {
-        key: '3',
-        name: '胡彦祖',
-        age: 42,
-        address: '西湖区湖底公园1号',
-      },
-  ];
-  
+// const dataSource = [
+//     {
+//       key: '1',
+//       name: '胡彦斌',
+//       age: 32,
+//       address: '西湖区湖底公园1号',
+//     },
+//     {
+//       key: '2',
+//       name: '胡彦祖',
+//       age: 42,
+//       address: '西湖区湖底公园1号',
+//     },
+//     {
+//         key: '3',
+//         name: '胡彦祖',
+//         age: 42,
+//         address: '西湖区湖底公园1号',
+//       },
+//   ];
   const columns = [
     {
       title: '姓名',
@@ -40,18 +39,31 @@ const dataSource = [
       dataIndex: 'address',
       key: 'address',
     },
+    {
+        title: '生日',
+        dataIndex: 'birthday',
+        key: 'birthday',
+      },
   ];
 class Student extends Component {
+    constructor(){
+        super();
+        this.state = {
+            dataSource:[]
+        }
+    }
     componentWillMount(){
         axios.get('http://www.weichuang.com/student').then((res)=>{
-            console.log(res);
+            this.setState({
+                dataSource:res.data.list
+            })
         })
     }
     render() {
         return (
             <div className="student-list">
                 学生列表
-                <Table dataSource={dataSource} columns={columns} />;
+                <Table dataSource={this.state.dataSource} columns={columns} />;
             </div>
         );
     }
